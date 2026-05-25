310d32c16e2157453e070c6964a9317d9ea9698a
Bwilliams1026 <123887573+Bwilliams1026@users.noreply.github.com>
2026-05-25 21:28:42 +0000

    First Commit

---
+++ about_me.py
@@ -0,0 +1,17 @@
+print("=" * 40)
+print("Student Profile")
+print("=" * 40)
+first_name = "Brandon"
+last_name = "Williams"
+age = 44
+city = "Hickory"
+job = "Scheduler"
+fun_fact = "I played college football"
+
+print("Name:", first_name, last_name)
+print("Age:", age)
+print("City:", city)
+print("Job:", job)
+print(f"I want a better career.")
+print("Fun Fact:", fun_fact)
+print("=" * 40)
